08e8dc0258e5824b5d0f99047d4570aef1325c2e
jannaescur <janna.escur@alu-etsetb.upc.edu>
2016-11-25 17:59:34 +0100

    LAYOUT CREATE EVENT

--- app/src/main/java/com/developer/janna/layouts/MainActivity.java
+++ app/src/main/java/com/developer/janna/layouts/MainActivity.java
@@ -4,18 +4,48 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
 import android.icu.util.Calendar;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
 import android.widget.DatePicker;
 import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+import static com.developer.janna.layouts.MainActivity.TimeEdit;
+import static com.developer.janna.layouts.R.id.imageView;
+
+public class MainActivity extends FragmentActivity {
+
+    private final int SELECT_PHOTO = 1;
+    private static int IMG_RESULT = 1;
+    String ImageDecode;
+    ImageView imageViewLoad;
+    Button LoadImage;
+    Intent intent;
+    String[] FILE;
 
 
     static EditText TimeEdit;
@@ -23,18 +53,69 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choose_avatar);
-/*
+        setContentView(R.layout.create_event);
+
         TimeEdit = (EditText) findViewById(R.id.editText1);
         TimeEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showTruitonTimePickerDialog(v);
                 showTruitonDatePickerDialog(v);
+
+
             }
-        });*/
+        });
+
+        imageViewLoad = (ImageView) findViewById(R.id.image_event);
+        LoadImage = (Button) findViewById(R.id.button_photo);
+
+        LoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+           /* public void onClick(View v) {
+
+                intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(intent, IMG_RESULT);
+
+            }*/
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
+
     }
-    /*
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        imageViewLoad.setImageBitmap(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+
     public void showTruitonDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
@@ -60,6 +141,7 @@ public class MainActivity extends Activity {
             TimeEdit.setText(day + "/" + (month + 1) + "/" + year);
         }
     }
+
     public void showTruitonTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
@@ -80,15 +162,18 @@ public class MainActivity extends Activity {
                     DateFormat.is24HourFormat(getActivity()));
         }
 
-       public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
             TimeEdit.setText(TimeEdit.getText() + "-" + hourOfDay + ":" + minute);
         }
     }
 
 
-*/
-    }
+}
+
+
+
+
 
 
 
