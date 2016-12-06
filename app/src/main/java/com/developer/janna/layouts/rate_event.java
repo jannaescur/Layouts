08d464bfe8bf5b5b54afaddb88a0647465293eff
jannaescur <janna.escur@alu-etsetb.upc.edu>
2016-12-06 13:37:11 +0100

    - el disseny està acabat, scroll funciona (create event) - nou layout: rate_event

---
+++ app/src/main/java/com/developer/janna/layouts/rate_event.java
@@ -0,0 +1,16 @@
+package com.developer.janna.layouts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by janna on 06/12/2016.
+ */
+
+public class rate_event extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rate_event);
+
+    }
+}
